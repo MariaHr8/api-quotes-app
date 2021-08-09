e3ded0d8bbb14f19a643bd6324abacad764e9aa4
Maria Hristova <fcmj@abv.bg>
2021-08-09 18:40:11 +0300

    Getting quotes from anime name

--- index.js
+++ index.js
@@ -2,26 +2,42 @@ let personName = document.getElementById('name');
 let animeName = document.getElementById('anime');
 let quoteName = document.getElementById('quote');
 
-fetch('https://animechan.vercel.app/api/random')
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+function getNew() {
+    fetch('https://animechan.vercel.app/api/random')
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status);
+                    return;
+                }
 
-      // Examine the text in the response
-      response.json().then(function(data) {
-        personName.innerHTML = data.character;
-        animeName.innerHTML = data.anime;
-        quoteName.innerHTML = data.quote;
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+                // Examine the text in the response
+                response.json()
+                    .then(function (data) {
+                        personName.innerHTML = data.character;
+                        animeName.innerHTML = data.anime;
+                        quoteName.innerHTML = data.quote;
+                    });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
 
+function loadAnimeQuotes(e) {
+    console.log(animeName.innerHTML);
+    fetch(`https://animechan.vercel.app/api/quotes/anime?title=` + animeName.innerHTML)
+        .then(response => {
+            if (response.status !== 200) {
+                console.log('A problem occured! Status code: ' + response.status);
+                return;
+            }
 
- 
+            response.json()
+                .then(data => {
+                    console.log(data);
+                })
+        }
+        )
+}
